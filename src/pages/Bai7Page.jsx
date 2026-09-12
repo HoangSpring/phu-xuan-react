bbb2c8db8434a6aa260b4b523632f08625103e2f
HoangSpring <levanhoang151203@gmail.com>
2026-09-12 19:39:46 +0700

    Lab 5: keo tha, phim tat, aria-live

--- src/pages/Bai7Page.jsx
+++ src/pages/Bai7Page.jsx
@@ -3,6 +3,7 @@ import DanhSachDiaDanh from '../components/Bai7/DanhSachDiaDanh';
 import KhamPhaDiaDanh from '../components/Bai7/KhamPhaDiaDanh';
 import LuotThichMonAn from '../components/Bai7/LuotThichMonAn';
 import TimMonAnHue from '../components/Bai7/TimMonAnHue';
+import MonAnYeuThich from '../components/Bai7/MonAnYeuThich';
 import '../styles/Bai7.css';
 
 export default function Bai7Page() {
@@ -14,6 +15,7 @@ export default function Bai7Page() {
       <KhamPhaDiaDanh />
       <LuotThichMonAn />
       <TimMonAnHue />
+      <MonAnYeuThich />
     </main>
   );
 }
