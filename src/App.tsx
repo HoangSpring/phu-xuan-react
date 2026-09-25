90f31d7123af3ed0f59c5bbbbb7b4d15fc81cb46
HoangSpring <levanhoang151203@gmail.com>
2026-09-25 09:17:07 +0700

    buoi9-lab1: tao component TheDiaDanh va hien thi danh sach bang map

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import TrangQuanLyDiaDiem from './features/dia-diem/TrangQuanLyDiaDiem';
+import TheDiaDanh from "./components/TheDiaDanh";
+import { DANH_SACH_DIA_DANH } from "./du-lieu/diaDanh";
+import "./App.css";
 
 function App() {
   return (
-    <div style={{ padding: '20px', maxWidth: '1000px', margin: '0 auto' }}>
-      <h1>Buổi 8: Quản lý biểu mẫu có kiểm soát</h1>
-      <TrangQuanLyDiaDiem />
+    <div className="trang">
+      <h1>Khám phá Cố đô Huế</h1>
+      <div className="luoi-dia-danh">
+        {DANH_SACH_DIA_DANH.map((dd) => (
+          <TheDiaDanh
+            key={dd.id}
+            anh={dd.anh}
+            ten={dd.ten}
+            moTa={dd.moTa}
+          />
+        ))}
+      </div>
     </div>
   );
 }
